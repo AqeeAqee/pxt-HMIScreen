f13576c084dbbaa2ca80aedd1c81922b0e126526
AqeeAqee <aqee@msn.com>
2022-01-09 12:34:29 +0800

    Update hmi.ts, hmi-instructions.ts, test.ts

--- test.ts
+++ test.ts
@@ -31,13 +31,15 @@ let index = 0
 serial.redirectToUSB()
 radio.setGroup(10)
 hmi.initialize(DeviceType.ta, CommunicationType.radio)
+radio.setTransmitPower(7)
 basic.showString("R")
-hmi.setColors(0x112233, 0xaabbcc)
+hmi.setColors(0xff00ff, 0x0000ff)
 //hmi.addToConsoleLogListener()
-hmi.setMinPriority(ConsolePriority.Log)
+hmi.setMinPriority(ConsolePriority.Debug)
 hmi.showPic(1)
-hmi.Hello()
+hmi.showQRCode("http://makecode.microbit.org", 3, 100, 30)
 hmi.log("===pxt-SerialScreen test==") // 
+hmi.Hello()
 index = 1
 basic.showIcon(IconNames.House)
 /*
@@ -55,13 +57,12 @@ function showNextImage() {
         else if (index > 3 ) {
             index = 0
         }
-        //hmi.showPic(index)
-        hmi.cutPasteImage(index, 0, 0, 333, 333, 10, 10, ImagePasteBgMode.source)
+        hmi.showPic(index)//, PicTransMode.m2s
+        //hmi.cutPasteImage(index, 0, 0, 333, 333, 10, 10, ImagePasteBgMode.source)
         
         //led.plotBarGraph(index,59)
         basic.showNumber(index,20)
     }
-    // hmi.showText("test" + convertToText(index), FontSize.fs32, 100,30)
     // hmi.showTextUnicode(
     //     convertToText(index),
     //     FontSizeUnicode.fs16,
@@ -80,8 +81,12 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     deltaPicID = 1
 })
-loops.everyInterval(50, ()=>{
-    showNextImage()
+let n=0
+loops.everyInterval(1000, ()=>{
+    //showNextImage()
+    // n++
+    // hmi.showText0(n.toString(), FontSize0.fs32, 100, 130)
+    // basic.showNumber(n,200)
 })
 hmi.onTouchDown(function (x, y) {
     serial.writeLine("TouchDown:" + x.toString() + "," + y.toString())
